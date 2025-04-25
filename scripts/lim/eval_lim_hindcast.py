f123ee4c06783309ea934ec73838502b56b7f4ba
Jakob Schloer <jakob.schloer@gmail.com>
2025-04-25 09:05:35 +0000

    Clean-up

--- scripts/lim/eval_lim_hindcast.py
+++ scripts/lim/eval_lim_hindcast.py
@@ -1,135 +1,143 @@
-''' Perfrom hindcast of LSTM model and compute verification metrics. 
+"""Perfrom hindcast of LSTM model and compute verification metrics.
 
-@Author  :   Jakob Schlör 
+@Author  :   Jakob Schlör
 @Time    :   2023/08/23 14:40:23
 @Contact :   jakob.schloer@uni-tuebingen.de
-'''
-# %%
-import os, argparse
+"""
+
+import argparse
+import os
+from importlib import reload
+
 import numpy as np
 import xarray as xr
-from hyblim.data import eof, preproc
-from hyblim.utils import metric, eval, enso
 
-from importlib import reload
+from hyblim.data import eof
+from hyblim.data import preproc
+from hyblim.utils import enso
+from hyblim.utils import eval
+from hyblim.utils import metric
+
 reload(metric)
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 
 
-def hindcast_evaluation(lim_hindcast: xr.DataArray, 
-                        ds_target: xr.Dataset, 
-                        combined_eof: eof.CombinedEOF, 
-                        lag_arr: list,
-                        add_eof_noise: bool = True):
-    """ Perform hindcast and compute verification metrics for LIM ensemble hindcasts.
+def hindcast_evaluation(
+    lim_hindcast: xr.DataArray,
+    ds_target: xr.Dataset,
+    combined_eof: eof.CombinedEOF,
+    lag_arr: list,
+    add_eof_noise: bool = True,
+) -> tuple[xr.Dataset, xr.Dataset, xr.Dataset]:
+    """Perform hindcast and compute verification metrics for LIM ensemble hindcasts.
 
     Args:
         lim_hindcast (xr.DataArray): Hindcast data.
         ds_target (xr.Dataset): Target dataset.
         combined_eof (eof.CombinedEOF): Combined EOF object
         lag_arr (list): List of lags to compute metrics for
-        scorepath (str): Path to save metrics
+        add_eof_noise (bool): Whether to add randomly generated higher order EOFs to the hindcast
+
+    Returns:
+        tuple[xr.Dataset, xr.Dataset, xr.Dataset]: Grid scores, time scores, nino indices
     """
     normalizers = {var: preproc.normalizer_from_dict(ds_target[var].attrs) for var in ds_target.data_vars}
 
-    # Extended PCA with 300 components 
+    # Extended PCA with 300 components
     if add_eof_noise:
-        n_components_full = 300 
+        n_components_full = 300
         eofa_list = []
         for i, var in enumerate(ds_target.data_vars):
             print(f"Create extended EOF of {var}!", flush=True)
-            eofa = eof.EmpiricalOrthogonalFunctionAnalysis(n_components=n_components_full, )
+            eofa = eof.EmpiricalOrthogonalFunctionAnalysis(
+                n_components=n_components_full,
+            )
             eofa.fit(ds_target[var])
             eofa_list.append(eofa)
         extended_eof = eof.CombinedEOF(eofa_list, vars=list(ds_target.data_vars))
     else:
         extended_eof = None
-    
-    lag_arr = params['lags']
-    verification_per_gridpoint, verification_per_time, nino_indices = [], [], [] 
+
+    lag_arr = params["lags"]
+    verification_per_gridpoint, verification_per_time, nino_indices = [], [], []
     for lag in lag_arr:
         # Transform hindcast to grid space
         print(f"Lag: {lag}", flush=True)
-        z = lim_hindcast.sel(lag=lag).data[:-lag,:,:]
+        z = lim_hindcast.sel(lag=lag).data[:-lag, :, :]
         print("Z shape: ", z.shape)
-        target_time = lim_hindcast['time'].data[lag:]
+        target_time = lim_hindcast["time"].data[lag:]
         print("Target time: ", target_time.shape)
-        x_hindcast = eval.latent_frcst_to_grid(
-            z, target_time, combined_eof, normalizers, extended_eof=extended_eof
-        )
+        x_hindcast = eval.latent_frcst_to_grid(z, target_time, combined_eof, normalizers, extended_eof=extended_eof)
 
         # Get target data and revert normalization
         x_target = ds_target.sel(time=target_time)
-        x_target = xr.merge([normalizers[var].inverse_transform(x_target[var])
-                             for var in x_target.data_vars])
+        x_target = xr.merge([normalizers[var].inverse_transform(x_target[var]) for var in x_target.data_vars])
 
         print("Compute metrics!", flush=True)
-        x_frcst_mean = x_hindcast.mean(dim='member')
-        x_frcst_std = x_hindcast.std(dim='member', ddof=1)
-        n_members = len(x_hindcast['member'])
+        x_frcst_mean = x_hindcast.mean(dim="member")
+        x_frcst_std = x_hindcast.std(dim="member", ddof=1)
+        n_members = len(x_hindcast["member"])
 
         # Compute metrics per gridpoint
-        grid_verif = metric.verification_metrics_per_gridpoint(
-            x_target, x_frcst_mean, x_frcst_std, n_members=n_members
-        )
-        grid_verif['lag'] = lag
+        grid_verif = metric.verification_metrics_per_gridpoint(x_target, x_frcst_mean, x_frcst_std, n_members=n_members)
+        grid_verif["lag"] = lag
         verification_per_gridpoint.append(grid_verif)
 
         # Compute metrics per time
-        time_verif = metric.verification_metrics_per_time(
-            x_target, x_frcst_mean, x_frcst_std, n_members=n_members
-        )
-        time_verif['lag'] = lag
+        time_verif = metric.verification_metrics_per_time(x_target, x_frcst_mean, x_frcst_std, n_members=n_members)
+        time_verif["lag"] = lag
         verification_per_time.append(time_verif)
 
         # Nino indices
         nino_index = {
-            'target': enso.get_nino_indices(x_target['ssta']),
-            'frcst': enso.get_nino_indices(x_hindcast['ssta']),
-            'lag': lag
+            "target": enso.get_nino_indices(x_target["ssta"]),
+            "frcst": enso.get_nino_indices(x_hindcast["ssta"]),
+            "lag": lag,
         }
         nino_indices.append(nino_index)
 
-        grid_scores = metric.listofdicts_to_dictofxr(verification_per_gridpoint, dim_key='lag')
-        time_scores = metric.listofdicts_to_dictofxr(verification_per_time, dim_key='lag')
-        nino_ids = metric.listofdicts_to_dictofxr(nino_indices, dim_key='lag')
+        grid_scores = metric.listofdicts_to_dictofxr(verification_per_gridpoint, dim_key="lag")
+        time_scores = metric.listofdicts_to_dictofxr(verification_per_time, dim_key="lag")
+        nino_ids = metric.listofdicts_to_dictofxr(nino_indices, dim_key="lag")
 
     return grid_scores, time_scores, nino_ids
 
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-path', '--path', type=str, required=True, help='Path to model')
-    parser.add_argument('-lags', '--lags', nargs='+', default=[1, 3, 6, 9, 12, 15, 18, 21, 24],
-                        help='Lags to compute metrics for.')
-    parser.add_argument('-datasplit', '--datasplit', type=str, default='test',
-                        help='Datasplit to compute metrics for.')
+    parser.add_argument("-path", "--path", type=str, required=True, help="Path to model")
+    parser.add_argument(
+        "-lags", "--lags", nargs="+", default=[1, 3, 6, 9, 12, 15, 18, 21, 24], help="Lags to compute metrics for."
+    )
+    parser.add_argument("-datasplit", "--datasplit", type=str, default="test", help="Datasplit to compute metrics for.")
     params = vars(parser.parse_args())
     return params
 
-# %%
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     params = argument_parser()
-    # Specify parameters
-    #params = {'path': '/home/ecm1922/Code/hybridLIM/models/lim/cslim_ssta-ssha/cslim_hindcast_ssta-ssha_eof20_test.nc',
-    #          'datasplit': 'test', 'lags': [1, 3]}
 
     # Load hindcast
-    lim_hindcast = xr.open_dataset(params['path'])['z'].sel(lag=slice(1, None))
+    lim_hindcast = xr.open_dataset(params["path"])["z"].sel(lag=slice(1, None))
 
     datapaths = {}
-    datapaths['ssta'] = PATH + "/../../data/cesm2-picontrol/b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.ssta_lat-31_33_lon130_290_gr1.0.nc"
-    datapaths['ssha'] = PATH + "/../../data/cesm2-picontrol/b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.ssha_lat-31_33_lon130_290_gr1.0.nc"
+    datapaths["ssta"] = (
+        PATH
+        + "/../../data/cesm2-picontrol/b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.ssta_lat-31_33_lon130_290_gr1.0.nc"
+    )
+    datapaths["ssha"] = (
+        PATH
+        + "/../../data/cesm2-picontrol/b.e21.B1850.f09_g17.CMIP6-piControl.001.pop.h.ssha_lat-31_33_lon130_290_gr1.0.nc"
+    )
     n_eof = [20, 10]
 
-    # %%
     # Load data
     print("Load data!", flush=True)
     da_arr = []
     for var, path in datapaths.items():
         da = xr.open_dataset(path)[var]
-        # Normalize data 
+        # Normalize data
         normalizer = preproc.Normalizer()
         da = normalizer.fit_transform(da)
         # Store normalizer as an attribute in the Dataarray for the inverse transformation
@@ -139,31 +147,27 @@ if __name__ == '__main__':
     ds = xr.merge(da_arr)
 
     # Apply land sea mask
-    lsm = xr.open_dataset(PATH+"/../../data/land_sea_mask_common.nc")['lsm']
-    ds = ds.where(lsm!=1, other=np.nan)
+    lsm = xr.open_dataset(PATH + "/../../data/land_sea_mask_common.nc")["lsm"]
+    ds = ds.where(lsm != 1, other=np.nan)
 
     # Create PCA
     eofa_lst = []
     for i, var in enumerate(ds.data_vars):
         print(f"Create EOF of {var}!")
-        n_components = n_eof[i] if isinstance(n_eof, list) else n_eof 
+        n_components = n_eof[i] if isinstance(n_eof, list) else n_eof
         eofa = eof.EmpiricalOrthogonalFunctionAnalysis(n_components)
-        eofa.fit(
-            ds[var].isel(time=slice(None, int(0.8*len(ds['time']))))
-        )
+        eofa.fit(ds[var].isel(time=slice(None, int(0.8 * len(ds["time"])))))
         eofa_lst.append(eofa)
     combined_eof = eof.CombinedEOF(eofa_lst, vars=list(ds.data_vars))
 
-    # %%
     # Perform hindcast evaluation
-    lag_arr = [int(lag) for lag in params['lags']]
+    lag_arr = [int(lag) for lag in params["lags"]]
     verification_per_gridpoint, verification_per_time, nino_indices = hindcast_evaluation(
         lim_hindcast, ds, combined_eof, lag_arr, add_eof_noise=False
     )
 
-    # %%
     # Save metrics to file
-    scorepath = os.path.join(os.path.dirname(params['path']) + "/metrics")
+    scorepath = os.path.join(os.path.dirname(params["path"]) + "/metrics")
     print("Save metrics to file!", flush=True)
     if not os.path.exists(scorepath):
         os.makedirs(scorepath)
@@ -174,4 +178,3 @@ if __name__ == '__main__':
         score.to_netcdf(scorepath + f"/timescore_{key}_{params['datasplit']}.nc")
     for key, nino_idx in nino_indices.items():
         nino_idx.to_netcdf(scorepath + f"/nino_{key}_{params['datasplit']}.nc")
-    # %%
--- scripts/limlstm/eval_limlstm.py
+++ scripts/limlstm/eval_limlstm.py
@@ -1,20 +1,26 @@
-''' Perfrom hindcast of LSTM model and compute verification metrics. 
+"""Perfrom hindcast of LSTM model and compute verification metrics.
 
-@Author  :   Jakob Schlör 
+@Author  :   Jakob Schlör
 @Time    :   2023/08/23 14:40:23
 @Contact :   jakob.schloer@uni-tuebingen.de
-'''
-# %%
-import os, argparse, json
-import torch
-import numpy as np
-import xarray as xr
-from tqdm import tqdm
-from hyblim.model import lstm
-from hyblim.data import eof, dataloader, preproc
-from hyblim.utils import metric, eval
+"""
 
+import argparse
+import json
+import os
 from importlib import reload
+
+import numpy as np
+import torch
+import xarray as xr
+
+from hyblim.data import dataloader
+from hyblim.data import eof
+from hyblim.data import preproc
+from hyblim.model import lstm
+from hyblim.utils import eval
+from hyblim.utils import metric
+
 reload(metric)
 
 PATH = os.path.dirname(os.path.abspath(__file__))
@@ -24,48 +30,55 @@ def hindcast(model, dataloader, normalizer_pca, device):
     targets, frcsts, time_ids = [], [], []
     with torch.no_grad():
         for lim_input, target, aux in dataloader:
-            x_input, x_target = lim_input.to(device), target.to(device) 
-            context = aux['month'].to(device, dtype=torch.long) 
+            x_input, x_target = lim_input.to(device), target.to(device)
+            context = aux["month"].to(device, dtype=torch.long)
             # Prediction
             x_ensemble = model(x_input, context)
 
             targets.append(x_target.cpu())
-            time_ids.append(aux['idx'])
+            time_ids.append(aux["idx"])
             frcsts.append(x_ensemble.cpu())
 
     time_idx = torch.cat(time_ids, dim=0).to(dtype=int).numpy()
     z_hindcast = {}
-    z_hindcast['target'] = torch.cat(targets, dim=0).unsqueeze(dim=1).numpy()
-    z_hindcast['frcst'] = torch.cat(frcsts, dim=0).numpy()
-    
+    z_hindcast["target"] = torch.cat(targets, dim=0).unsqueeze(dim=1).numpy()
+    z_hindcast["frcst"] = torch.cat(frcsts, dim=0).numpy()
+
     # Unnormalize in PCA space
     for key, z_norm in z_hindcast.items():
         n_time, n_member, n_lag, n_components = z_norm.shape
         # Convert to xarray with eof dimension to unnormalize each eof seperately
-        z_norm = xr.DataArray(z_norm, coords=dict(
-            n_time=np.arange(n_time), n_member=np.arange(n_member),
-            lag=np.arange(1, n_lag+1), eof=np.arange(1, n_components+1)
-        ))
+        z_norm = xr.DataArray(
+            z_norm,
+            coords=dict(
+                n_time=np.arange(n_time),
+                n_member=np.arange(n_member),
+                lag=np.arange(1, n_lag + 1),
+                eof=np.arange(1, n_components + 1),
+            ),
+        )
         z = np.zeros((n_time, n_member, n_lag, n_components))
         for i in range(n_member):
             for j in range(n_lag):
-                z[:,i, j, :] = normalizer_pca.inverse_transform(z_norm[:, i, j, :])
+                z[:, i, j, :] = normalizer_pca.inverse_transform(z_norm[:, i, j, :])
         z_hindcast[key] = z
-    
-    z_hindcast['target'] = z_hindcast['target'].squeeze()
+
+    z_hindcast["target"] = z_hindcast["target"].squeeze()
     return z_hindcast, time_idx
 
 
-def perform_hindcast_evaluation(model: torch.nn.Module, 
-                                checkpoint: dict, 
-                                ds: xr.Dataset, 
-                                dataloaders: torch.utils.data.DataLoader,
-                                datasplit: str,
-                                scaler_pca: preproc.Normalizer, 
-                                combined_eof: eof.CombinedEOF, 
-                                lag_arr: list, 
-                                scorepath: str):
-    """ Perform hindcast and compute verification metrics for LSTM model.
+def perform_hindcast_evaluation(
+    model: torch.nn.Module,
+    checkpoint: dict,
+    ds: xr.Dataset,
+    dataloaders: torch.utils.data.DataLoader,
+    datasplit: str,
+    scaler_pca: preproc.Normalizer,
+    combined_eof: eof.CombinedEOF,
+    lag_arr: list,
+    scorepath: str,
+) -> None:
+    """Perform hindcast and compute verification metrics for LSTM model.
 
     Args:
         model (torch.nn.Module): LSTM model
@@ -78,35 +91,37 @@ def perform_hindcast_evaluation(model: torch.nn.Module,
         lag_arr (list): List of lags to compute metrics for
         scorepath (str): Path to save metrics
     """
-    model.load_state_dict(checkpoint['model_state_dict'])
-    device= torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model.load_state_dict(checkpoint["model_state_dict"])
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     _ = model.to(device)
 
     # Hindcast in latent space
     z_hindcast, time_idx = hindcast(model, dataloaders[datasplit], scaler_pca, device)
 
-    # Extended PCA with 300 components 
-    n_components_full = 300 
+    # Extended PCA with 300 components
+    n_components_full = 300
     eofa_list = []
     for i, var in enumerate(ds.data_vars):
         print(f"Create extended EOF of {var}!", flush=True)
-        eofa = eof.EmpiricalOrthogonalFunctionAnalysis(n_components=n_components_full, )
+        eofa = eof.EmpiricalOrthogonalFunctionAnalysis(
+            n_components=n_components_full,
+        )
         eofa.fit(ds[var])
         eofa_list.append(eofa)
     extended_eof = eof.CombinedEOF(eofa_list, vars=list(ds.data_vars))
 
     # Verification metrics
-    times = dataloaders[datasplit].dataset.data['time'].data
+    times = dataloaders[datasplit].dataset.data["time"].data
     ds_target = ds.sel(time=times)
     verification_per_gridpoint, verification_per_time, nino_indices = eval.latent_evaluation(
-        z_hindcast['frcst'], time_idx, times, combined_eof, ds_target, lag_arr, extended_eof  
+        z_hindcast["frcst"], time_idx, times, combined_eof, ds_target, lag_arr, extended_eof
     )
 
     # Save metrics to file
     print("Save metrics to file!", flush=True)
     if not os.path.exists(scorepath):
         os.makedirs(scorepath)
-    
+
     for key, score in verification_per_gridpoint.items():
         score.to_netcdf(scorepath + f"/gridscore_{key}_{datasplit}.nc")
     for key, score in verification_per_time.items():
@@ -119,51 +134,48 @@ def perform_hindcast_evaluation(model: torch.nn.Module,
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-path', '--model_path', type=str, required=True, help='Path to model')
-    parser.add_argument('-datasplit', '--datasplit', type=str, default='test', help='Datasplit to evaluate')
-    parser.add_argument('-lags', '--lags', nargs='+', default=[1, 3, 6, 9, 12, 15, 18, 21, 24],
-                        help='Lags to compute metrics for.')
+    parser.add_argument("-path", "--model_path", type=str, required=True, help="Path to model")
+    parser.add_argument("-datasplit", "--datasplit", type=str, default="test", help="Datasplit to evaluate")
+    parser.add_argument(
+        "-lags", "--lags", nargs="+", default=[1, 3, 6, 9, 12, 15, 18, 21, 24], help="Lags to compute metrics for."
+    )
     params = vars(parser.parse_args())
     return params
 
-# %%
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Specify parameters
     params = argument_parser()
-    #params = {'model_path': "/home/ecm1922/Code/hybridLIM/models/limlstm/33119203_LIM-LSTM_ssta_n20_ssha_n10_g0.65-crps_member16_nhoriz_20_layers_1_latent64_cosinelr0.001-1e-06_bs64",
-    #          'datasplit': 'test', 'lags': [1]}
-    with open(params['model_path'] + "/config.json", 'r') as f:
+    with open(params["model_path"] + "/config.json", "r") as f:
         config = json.load(f)
 
-
     # Load data
-    lim_hindcast = { 
-       'train': xr.open_dataset(config['lim_path'] + "_train.nc")['z'].sel(lag=slice(1, None)),
-       'val': xr.open_dataset(config['lim_path'] + "_val.nc")['z'].sel(lag=slice(1, None)),
-       'test': xr.open_dataset(config['lim_path'] + "_test.nc")['z'].sel(lag=slice(1, None)),
+    lim_hindcast = {
+        "train": xr.open_dataset(config["lim_path"] + "_train.nc")["z"].sel(lag=slice(1, None)),
+        "val": xr.open_dataset(config["lim_path"] + "_val.nc")["z"].sel(lag=slice(1, None)),
+        "test": xr.open_dataset(config["lim_path"] + "_test.nc")["z"].sel(lag=slice(1, None)),
     }
 
     # Create dataset
     ds, datasets, dataloaders, combined_eofa, normalizer_pca = dataloader.load_pcdata_lim_ensemble(
         lim_hindcast, **config
-    ) 
+    )
 
     # Define and load model
-    num_condition = 12 if config['film'] else -1
+    num_condition = 12 if config["film"] else -1
     model = lstm.ResidualLSTM(
-        input_dim=combined_eofa.n_components, hidden_dim=config['hidden_dim'],
-        num_conditions=num_condition, num_layers=config['layers'],
-        T_max=config['chrono']
+        input_dim=combined_eofa.n_components,
+        hidden_dim=config["hidden_dim"],
+        num_conditions=num_condition,
+        num_layers=config["layers"],
+        T_max=config["chrono"],
     )
 
     # Load model with best loss
-    checkpoint = torch.load(params['model_path'] + "/min_checkpoint.pt")
+    checkpoint = torch.load(params["model_path"] + "/min_checkpoint.pt")
 
-    lag_arr = [int(lag) for lag in params['lags']]
-    scorepath = params['model_path'] + "/metrics"
+    lag_arr = [int(lag) for lag in params["lags"]]
+    scorepath = params["model_path"] + "/metrics"
     perform_hindcast_evaluation(
-        model, checkpoint, ds, dataloaders, params['datasplit'], normalizer_pca, combined_eofa, lag_arr, scorepath
-    ) 
-
-
-# %%
+        model, checkpoint, ds, dataloaders, params["datasplit"], normalizer_pca, combined_eofa, lag_arr, scorepath
+    )
